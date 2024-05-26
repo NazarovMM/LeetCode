c5dd570cf06cea94ced64b5ecc75e787208de850
NazarovMM <maxzbox1@rambler.ru>
2024-05-26 13:25:35 +0300

    58. Length of Last Word

--- 20.py
+++ 20.py
@@ -1,23 +1,27 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        flag = True
-        while s != "":
-            if s[0] == "(" and ")" in s:
-                s = s[2:]
-            elif s[0] == "[" and "]" in s:
-                s = s[2:]
-            elif s[0] == "{" and "}" in s:
-                s = s[2:]
+        krygl = 0
+        kvadr = 0
+        figur = 0
+        for i in s:
+            if i == "(":
+                krygl+=1
+            elif i ==")":
+                krygl-=1
+            elif i=="[":
+                kvadr+=1
+            elif i=="]":
+                kvadr-=1
+            elif i=="{":
+                figur+=1
             else:
-                flag = False
-                break
-        if flag:
+                figur-=1
+        if krygl == kvadr == figur == 0:
             return True
         else:
             return False
 
-
-s = "(]"
+s = "([)]"
 solution = Solution()
 result = solution.isValid(s)
 print(result)
--- test.py
+++
@@ -1,6 +0,0 @@
-x = 26
-y = x**0.5
-print((y)//1)
-print(y**2)
-print(int(x/2)+1)
-print("erefe")
