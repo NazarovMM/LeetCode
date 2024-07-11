5425b9ae3cd1c56689d9241940a9f34f89281581
NazarovMM <maxzbox1@rambler.ru>
2024-07-11 13:43:55 +0300

    1190. Reverse Substrings Between Each Pair of Parentheses

---
+++ 1190.py
@@ -0,0 +1,23 @@
+class Solution:
+    def reverseParentheses(self, s: str) -> str:
+        stack = []
+        for char in s:
+            if char == ')':
+                rev = ""
+                while stack and stack[-1] != '(':
+                    rev += stack.pop()
+                if stack:
+                    stack.pop()
+                for c in rev:
+                    stack.append(c)
+            else:
+                stack.append(char)
+
+        return ''.join(stack)
+
+
+s = "(ed(et(oc))el)"
+#s = "(u(love)i)"
+solution = Solution()
+result = solution.reverseParentheses(s)
+print(result)
