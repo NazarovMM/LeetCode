13fa3b686489a5769d62c705cca9431c06f34ca9
NazarovMM <maxzbox1@rambler.ru>
2024-09-09 17:13:07 +0300

    fix 912. Sort an Array

--- 912.py
+++ 912.py
@@ -1,1 +1,32 @@
-g
+class Solution:
+    def sortArray(self, nums: list[int]) -> list[int]:
+        if len(nums) > 1:
+
+            r = len(nums) // 2
+            L = nums[:r]
+            M = nums[r:]
+
+            Solution().sortArray(L)
+            Solution().sortArray(M)
+
+            i = j = k = 0
+
+            while i < len(L) and j < len(M):
+                if L[i] < M[j]:
+                    nums[k] = L[i]
+                    i += 1
+                else:
+                    nums[k] = M[j]
+                    j += 1
+                k += 1
+
+            while i < len(L):
+                nums[k] = L[i]
+                i += 1
+                k += 1
+
+            while j < len(M):
+                nums[k] = M[j]
+                j += 1
+                k += 1
+        return nums
